14f8abf1d2f2abc58e068b3b9475ee10b1969673
Freek van de Ven <FJM.vandeVen@student.han.nl>
2022-02-19 22:47:12 +0100

    wakelock enabled

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:distributed/src/app.dart';
 import 'package:distributed/src/dependencies.dart';
 import 'package:flutter/material.dart';
+import 'package:wakelock/wakelock.dart';
 
-void main(){
+void main() {
+  Wakelock.enable();
   runApp(const DependencyProvider(child: ChipsDistributionGameApp()));
 }
